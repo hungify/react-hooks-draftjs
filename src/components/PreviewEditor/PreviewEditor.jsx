d193b855cf96d594e6e1b361c0d9a6defad33e7a
hung purdie <nmhung.dev@gmail.com>
2022-04-09 21:36:45 +0700

    style: simple ui for preview data

---
+++ src/components/PreviewEditor/PreviewEditor.jsx
@@ -0,0 +1,41 @@
+import { convertFromRaw, Editor, convertToRaw, EditorState } from "draft-js";
+import React from "react";
+import { linkDecorator } from "../Link/Link";
+import styled from "styled-components";
+const Wrapper = styled.div`
+  background: #dddddd;
+  color: #006778;
+  font-size: 2rem;
+  width: 100%;
+  height: 100%;
+  min-height: 500px;
+`;
+const Title = styled.div`
+  padding: 10px;
+  width: 100%;
+  text-align: center;
+  border-bottom: 1px solid #ccc;
+`;
+const WrapEditorPreview = styled.div`
+  padding: 10px;
+`;
+const convertToEditorContent = (editorState) => {
+  if (editorState.getCurrentContent().hasText()) {
+    const editorStateRaw = convertToRaw(editorState.getCurrentContent());
+    return EditorState.createWithContent(convertFromRaw(editorStateRaw), linkDecorator());
+  }
+  return EditorState.createEmpty();
+};
+
+export default function PreviewEditor({ dataRender }) {
+  const contentState = convertToEditorContent(dataRender, linkDecorator());
+
+  return (
+    <Wrapper>
+      <Title>Preview</Title>
+      <WrapEditorPreview>
+        <Editor editorState={contentState} readOnly={true} />
+      </WrapEditorPreview>
+    </Wrapper>
+  );
+}
