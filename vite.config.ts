c2a04b97650a0a8d3c4df0e4517fa105a3066b7d
hungify <nmhung.dev@gmail.com>
2022-08-03 15:00:21 +0700

    chore: change outDir build in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,13 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  build: {
+    outDir: './build',
+  },
   server: {
     port: 3000,
   },
   define: {
-    // By default, Vite doesn't include shims for NodeJS/
-    // necessary for segment analytics lib to work
     global: 'window',
   },
 });
